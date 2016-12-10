1fd436e314c445f442cc6b917ed63a6d4f289079
Igor Yegorov <iegoroff@gmail.com>
2016-12-10 18:47:09 +0300

    updated browser tests

--- tests/intern-browser.ts
+++ tests/intern-browser.ts
@@ -1,52 +1,54 @@
 export * from './intern';
 
 export const environments = [
-        {
-            browserName: 'iphone',
-            recordVideo: false,
-            recordScreenshots: false
-        },
-        {
-            browserName: 'android',
-            recordVideo: false,
-            recordScreenshots: false
-        },
-        {
-            browserName: 'chrome',
-            version: ['33', '48'],
-            platform: ['Linux', 'Windows 10', 'OS X 10.11'],
-            recordVideo: false,
-            recordScreenshots: false
-        },
-        {
-            browserName: 'firefox',
-            version: ['29', '45'],
-            platform: ['Linux', 'Windows 10', 'OS X 10.11'],
-            recordVideo: false,
-            recordScreenshots: false
-        },
-        {
-            browserName: 'MicrosoftEdge',
-            version: ['13', '14'],
-            platform: 'Windows 10',
-            recordVideo: false,
-            recordScreenshots: false
-        },
-        {
-            browserName: 'internet explorer',
-            version: ['10', '11'],
-            platform: 'Windows 7',
-            recordVideo: false,
-            recordScreenshots: false
-        },
-        {
-            browserName: 'safari',
-            version: ['9', '10'],
-            platform: 'OS X 10.11',
-            recordVideo: false,
-            recordScreenshots: false
-        }
-    ];
+    {
+        browserName: 'iphone',
+        version: ['8.1', '9.2'],
+        recordVideo: false,
+        recordScreenshots: false
+    },
+    {
+        browserName: 'android',
+        version: ['4.4', '5.1'],
+        recordVideo: false,
+        recordScreenshots: false
+    },
+    {
+        browserName: 'chrome',
+        version: ['53', '54'],
+        platform: ['Windows 10'],
+        recordVideo: false,
+        recordScreenshots: false
+    },
+    {
+        browserName: 'firefox',
+        version: ['49', '50'],
+        platform: ['Windows 10'],
+        recordVideo: false,
+        recordScreenshots: false
+    },
+    {
+        browserName: 'MicrosoftEdge',
+        version: ['13', '14'],
+        platform: 'Windows 10',
+        recordVideo: false,
+        recordScreenshots: false
+    },
+    {
+        browserName: 'internet explorer',
+        version: ['10', '11'],
+        platform: 'Windows 7',
+        recordVideo: false,
+        recordScreenshots: false
+    },
+    {
+        browserName: 'safari',
+        version: ['9', '10'],
+        platform: 'OS X 10.11',
+        recordVideo: false,
+        recordScreenshots: false
+    }
+];
 
 export const tunnel = 'SauceLabsTunnel';
 
