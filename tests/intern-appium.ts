a57d0a8082670eaf36d58788af6e34896cc4d23d
Igor Yegorov <iegoroff@gmail.com>
2017-05-29 02:45:15 +0300

    local appium

---
+++ tests/intern-appium.ts
@@ -0,0 +1,22 @@
+export * from './intern';
+
+export const environments = [
+  {
+    browserName: 'Chrome',
+    version: '6.0',
+    platform: 'Android',
+    platformName: 'Android',
+    deviceName: 'j7xelte'
+  }
+];
+
+export const tunnel = 'NullTunnel';
+
+export const tunnelOptions = {
+  hostname: '0.0.0.0',
+  port: 4723
+};
+
+export const proxyUrl = 'http://192.168.1.180:9000/';
+
+export const reporters = ['Runner'];
