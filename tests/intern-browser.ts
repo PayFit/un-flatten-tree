936e3cba21d5607db57b847b439d8224419e9d80
Igor Yegorov <iegoroff@gmail.com>
2017-05-28 03:23:13 +0300

    changed saucelabs to testingbot - 7

--- tests/intern-browser.ts
+++ tests/intern-browser.ts
@@ -1,68 +1,76 @@
 export * from './intern';
 
+function env(env: Object) {
+    return {
+        ...env,
+        ...{ screenshot: false, screenrecorder: false }
+    };
+}
+
 export const environments = [
-    {
+    env({
         browserName: 'chrome',
-        version: ['latest-1', 'latest-2'],
-        platform: 'WIN10',
-        screenshot: false,
-        screenrecorder: false
-    }
-    // {
-    //     browserName: 'Safari',
-    //     appiumVersion: '1.6.3',
-    //     recordVideo: false,
-    //     recordScreenshots: false,
-    //     deviceName: 'iPhone Simulator',
-    //     deviceOrientation: 'portrait',
-    //     platformVersion: ['9.3', '10.2'],
-    //     platformName: 'iOS'
-    // },
-    // {
-    //     browserName: 'Browser',
-    //     appiumVersion: '1.6.3',
-    //     recordVideo: false,
-    //     recordScreenshots: false,
-    //     deviceName: 'Android Emulator',
-    //     deviceOrientation: 'portrait',
-    //     platformVersion: ['5.1', '6.0'],
-    //     platformName: 'Android'
-    // },
-    // {
-    //     browserName: 'chrome',
-    //     version: ['56', '57'],
-    //     platform: ['Windows 10'],
-    //     recordVideo: false,
-    //     recordScreenshots: false
-    // },
-    // {
-    //     browserName: 'firefox',
-    //     version: ['51', '52'],
-    //     platform: ['Windows 10'],
-    //     recordVideo: false,
-    //     recordScreenshots: false
-    // },
-    // {
-    //     browserName: 'MicrosoftEdge',
-    //     version: ['13', '14'],
-    //     platform: 'Windows 10',
-    //     recordVideo: false,
-    //     recordScreenshots: false
-    // },
-    // {
-    //     browserName: 'internet explorer',
-    //     version: ['10', '11'],
-    //     platform: 'Windows 7',
-    //     recordVideo: false,
-    //     recordScreenshots: false
-    // },
-    // {
-    //     browserName: 'safari',
-    //     version: ['9', '10'],
-    //     platform: 'OS X 10.11',
-    //     recordVideo: false,
-    //     recordScreenshots: false
-    // }
+        version: ['58', '57'],
+        platform: 'WIN10'
+    }),
+    env({
+        browserName: 'firefox',
+        version: ['53', '52'],
+        platform: 'WIN10'
+    }),
+    env({
+        browserName: 'microsoftedge',
+        version: ['14', '13'],
+        platform: 'WIN10'
+    }),
+    env({
+        browserName: 'opera',
+        version: ['44', '43'],
+        platform: 'WIN10'
+    }),
+    env({
+        browserName: 'internet explorer',
+        version: ['11', '10'],
+        platform: 'WIN8'
+    }),
+    env({
+        browserName: 'safari',
+        version: '10',
+        platform: 'SIERRA'
+    }),
+    env({
+        browserName: 'safari',
+        version: '9',
+        platform: 'CAPITAN'
+    }),
+    env({
+        browserName: 'safari',
+        version: '10.3',
+        platform: 'SIERRA',
+        deviceName: 'iPhone 6s Plus',
+        platformName: 'iOS'
+    }),
+    env({
+        browserName: 'safari',
+        version: '9.3',
+        platform: 'CAPITAN',
+        deviceName: 'iPhone 6s Plus',
+        platformName: 'iOS'
+    }),
+    env({
+        browserName: 'Chrome',
+        version: '7.1',
+        platform: 'ANDROID',
+        deviceName: 'Nexus 7',
+        platformName: 'Android'
+    }),
+    env({
+        browserName: 'browser',
+        version: '6.0',
+        platform: 'ANDROID',
+        deviceName: 'Galaxy S6',
+        platformName: 'Android'
+    })
 ];
 
 export const tunnel = 'TestingBotTunnel';
